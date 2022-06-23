8f67dc90810d268cd758859432cc5f7a6b12147e
Heidi V <sharedmacc1@gmail.com>
2022-06-23 16:03:04 -0600

    updated header in team, stil in progress

--- client/src/components/team/TeamHeader.js
+++ client/src/components/team/TeamHeader.js
@@ -10,10 +10,10 @@ const TeamHeader = () => {
 			<br/>
 			<Container fluid="lg">
 				<Row>
-					<Col className="display-1 font-weight-bold" lg="auto"> 
+					<Col className="display-1 text-center font-weight-bold" lg="auto"> 
 						<h1>Meet The Team</h1> 
 					</Col>
-					<Col>
+					<Col className="display-6" sm="auto">
 						<hr className="my-4 bg-black" />
 						<p className="font-weight-dark mx-5" >
 						This can be the site slogan, or a brief description of the site. This can also be a call to action used to grab the attention of the visitors and get them to click on the link below.
